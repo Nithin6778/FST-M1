9cf70f756e51400ee2ea4c62c8d8c73c14d868fd
Srilakshmi-poka <srilpok1@in.ibm.com>
2024-06-26 14:27:19 +0530

    Add files via upload

---
+++ JUnit/Activities/Activity2.java
@@ -0,0 +1,28 @@
+package com.ibm.junit.fst_m1_junit;
+
+import org.junit.jupiter.api.Test;
+import activityPrograms.BankAccount;
+import activityPrograms.NotEnoughFundsException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpectedExceptionTest<BankAccount> {
+
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(NotEnoughFundsException.class, () -> ((Object) account).withdraw(10),
+                "Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+    
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
