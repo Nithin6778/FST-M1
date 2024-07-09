f5e97d6f0341b96d526b5cd59790e279dce1d9d4
Nithin6778 <Kunchakuri.Nithin@ibm.com>
2024-07-09 19:36:28 +0530

    Add files via upload

---
+++ SQL/Activities/Activity9,10,11.sql
@@ -0,0 +1,241 @@
+REM   Script: Activity,9,10,11
+REM   Activity9,10,11
+
+REM   Script: Session 01 
+
+
+REM   Activity1, Activity2, Activity3, Activity4, Activity5 
+
+
+CREATE TABLE salesman(salesman_id int PRIMARY KEY, salesman_name varchar2(32) not null, salesman_city varchar2(32), commission int);
+
+DESCRIBE salesman 
+
+
+INSERT ALL  
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11)  
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14)  
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13)  
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12)  
+SELECT 1 FROM DUAL;
+
+Select * from salesman;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=100;
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
+REM   Script: Activity6,7,8 
+
+
+REM   Activity6,7,8 
+
+
+create table orders(  
+    order_no int primary key, purchase_amount float, order_date date,  
+    customer_id int, salesman_id int);
+
+INSERT ALL  
+    INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002)   
+    INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005)  
+    INTO orders VALUES(70002, 65.26, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70004, 110.5, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70007, 948.5, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3005, 5002)  
+    INTO orders VALUES(70005, 2400.6, TO_DATE('2012/07/27', 'YYYY/MM/DD'), 3007, 5001)  
+    INTO orders VALUES(70008, 5760, TO_DATE('2012/08/15', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70010, 1983.43, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3004, 5006)  
+    INTO orders VALUES(70003, 2480.4, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70012, 250.45, TO_DATE('2012/06/27', 'YYYY/MM/DD'), 3008, 5002)  
+    INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007)  
+    INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001)  
+SELECT 1 FROM DUAL;
+
+SELECT * from orders;
+
+create table orders(  
+    order_no int primary key, purchase_amount float, order_date date,  
+    customer_id int, salesman_id int);
+
+INSERT ALL  
+    INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002)   
+    INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005)  
+    INTO orders VALUES(70002, 65.26, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70004, 110.5, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70007, 948.5, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3005, 5002)  
+    INTO orders VALUES(70005, 2400.6, TO_DATE('2012/07/27', 'YYYY/MM/DD'), 3007, 5001)  
+    INTO orders VALUES(70008, 5760, TO_DATE('2012/08/15', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70010, 1983.43, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3004, 5006)  
+    INTO orders VALUES(70003, 2480.4, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70012, 250.45, TO_DATE('2012/06/27', 'YYYY/MM/DD'), 3008, 5002)  
+    INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007)  
+    INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001)  
+SELECT 1 FROM DUAL;
+
+SELECT * from orders;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+SELECT SUM(purchase_amount) AS "Total sum" FROM ORDERS;
+
+SELECT AVG(purchase_amount) AS "Average" FROM ORDERS;
+
+SELECT MAX(purchase_amount) AS "Maximum" FROM ORDERS;
+
+SELECT MIN(purchase_amount) AS "Minumum" FROM ORDERS;
+
+SELECT COUNT(distinct salesman_id) AS "Total count" FROM ORDERS;
+
+SELECT * from orders;
+
+select distinct salesman_id from orders;
+
+SELECT CUSTOMER_ID, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS GROUP BY CUSTOMER_ID;
+
+SELECT SALESMAN_ID, ORDER_DATE, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS WHERE ORDER_DATE=TO_DATE('2012-08-17'),  
+GROUP BY SALESMAN_ID, ORDER_DATE;
+
+SELECT SALESMAN_ID, ORDER_DATE, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS WHERE ORDER_DATE=TO_DATE('2012-08-17','YYYY/MM/DD'),  
+GROUP BY SALESMAN_ID, ORDER_DATE;
+
+SELECT SALESMAN_ID, ORDER_DATE, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS WHERE ORDER_DATE=TO_DATE('2012-08-17','YYYY/MM/DD'),  
+GROUP BY SALESMAN_ID, ORDER_DATE;
+
+SELECT SALESMAN_ID, ORDER_DATE, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS   
+WHERE ORDER_DATE=TO_DATE('2012-08-17','YYYY/MM/DD') GROUP BY SALESMAN_ID, ORDER_DATE;
+
+SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id, order_date  
+HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
+REM   Script: Activity9,10,11 
+
+
+REM   Activity9,10,11 
+
+
+create table orders(  
+    order_no int primary key, purchase_amount float, order_date date,  
+    customer_id int, salesman_id int);
+
+INSERT ALL  
+    INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002)   
+    INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005)  
+    INTO orders VALUES(70002, 65.26, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70004, 110.5, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70007, 948.5, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3005, 5002)  
+    INTO orders VALUES(70005, 2400.6, TO_DATE('2012/07/27', 'YYYY/MM/DD'), 3007, 5001)  
+    INTO orders VALUES(70008, 5760, TO_DATE('2012/08/15', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70010, 1983.43, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3004, 5006)  
+    INTO orders VALUES(70003, 2480.4, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70012, 250.45, TO_DATE('2012/06/27', 'YYYY/MM/DD'), 3008, 5002)  
+    INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007)  
+    INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001)  
+SELECT 1 FROM DUAL;
+
+SELECT * from orders;
+
+create table orders(  
+    order_no int primary key, purchase_amount float, order_date date,  
+    customer_id int, salesman_id int);
+
+INSERT ALL  
+    INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002)   
+    INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005)  
+    INTO orders VALUES(70002, 65.26, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70004, 110.5, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70007, 948.5, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3005, 5002)  
+    INTO orders VALUES(70005, 2400.6, TO_DATE('2012/07/27', 'YYYY/MM/DD'), 3007, 5001)  
+    INTO orders VALUES(70008, 5760, TO_DATE('2012/08/15', 'YYYY/MM/DD'), 3002, 5001)  
+    INTO orders VALUES(70010, 1983.43, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3004, 5006)  
+    INTO orders VALUES(70003, 2480.4, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3009, 5003)  
+    INTO orders VALUES(70012, 250.45, TO_DATE('2012/06/27', 'YYYY/MM/DD'), 3008, 5002)  
+    INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007)  
+    INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001)  
+SELECT 1 FROM DUAL;
+
+SELECT * from orders;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+SELECT SUM(purchase_amount) AS "Total sum" FROM ORDERS;
+
+SELECT AVG(purchase_amount) AS "Average" FROM ORDERS;
+
+SELECT MAX(purchase_amount) AS "Maximum" FROM ORDERS;
+
+SELECT MIN(purchase_amount) AS "Minumum" FROM ORDERS;
+
+SELECT COUNT(distinct salesman_id) AS "Total count" FROM ORDERS;
+
+SELECT * from orders;
+
+select distinct salesman_id from orders;
+
+SELECT CUSTOMER_ID, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS GROUP BY CUSTOMER_ID;
+
+SELECT SALESMAN_ID, ORDER_DATE, MAX(PURCHASE_AMOUNT) AS "MAX AMOUNT" FROM ORDERS WHERE ORDER_DATE=TO_DATE('2012-08-17','YYYY/MM/DD'),  
+GROUP BY SALESMAN_ID, ORDER_DATE;
+
