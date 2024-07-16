2a20ebcebe4cbb9276a4ab7e40bd9811cf69f23a
Srilakshmi-poka <srilpok1@in.ibm.com>
2024-07-16 13:28:28 +0530

    Update Activity2.java

--- JUnit/Activities/Activity2.java
+++ JUnit/Activities/Activity2.java
@@ -1,28 +1,31 @@
 package com.ibm.junit.fst_m1_junit;
 
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
-import activityPrograms.BankAccount;
-import activityPrograms.NotEnoughFundsException;
-import static org.junit.jupiter.api.Assertions.*;
 
-class ExpectedExceptionTest<BankAccount> {
 
-    @Test
-    void notEnoughFunds() {
-        // Create an object for BankAccount class
-        BankAccount account = new BankAccount(9);
 
-        // Assertion for exception
-        assertThrows(NotEnoughFundsException.class, () -> ((Object) account).withdraw(10),
-                "Balance must be greater than amount of withdrawal");
-    }
-
-    @Test
-    void enoughFunds() {
-        // Create an object for BankAccount class
-        BankAccount account = new BankAccount(100);
-    
-        // Assertion for no exceptions
-        assertDoesNotThrow(() -> account.withdraw(100));
-    }
+public class Activity2 {
+	
+@Test
+void notEnoughFunds() {
+    // Create an object for BankAccount class
+ BankAccount account = new BankAccount(9);
+ assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10));
 }
+
+@Test
+void enoughFunds() {
+// Create an object for BankAccount class
+BankAccount account = new BankAccount(100);
+
+// Assertion for no exceptions
+assertDoesNotThrow(() -> account.withdraw(100));
+}
+
+}
+
+
