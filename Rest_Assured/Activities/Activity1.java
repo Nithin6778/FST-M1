c762f93205da00adad961c11003e38f47cef31ca
Srilakshmi-poka <srilpok1@in.ibm.com>
2024-07-24 18:37:03 +0530

    Update Activity1.java

--- Rest_Assured/Activities/Activity1.java
+++ Rest_Assured/Activities/Activity1.java
@@ -1,53 +1,56 @@
 package Activities;
 
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class Activity1 {
-	
-	final static String Root_URI = "https://petstore.swagger.io/v2/pet";
-	@Test(priority = 1)
-	public void addNewPet() {
-		String body ="{\r\n"
-				+ "  \"id\": 77232,\r\n"
-				+ "  \"name\": \"Riley\",\r\n"
-				+ "  \"status\": \"alive\"\r\n"
-				+ "}";
-		Response response =
-				given().contentType(ContentType.JSON)
-				.body(body)
-				.when().post(Root_URI);
-		
-		//assertion
-		System.out.println("Adding pet");
-		System.out.println(response.getBody().asPrettyString());
-	}
-	
-	@Test(priority = 2)
-	public void getPetInfo() {
-		Response response =
-				given().contentType(ContentType.JSON)
-				.when().pathParam("petId", 77232)
-				.get(Root_URI+"/{petId}");
-		//Assertion 	
-		System.out.println("Getting pet");
-		System.out.println(response.getBody().asPrettyString());
-	}
-	@Test(priority = 3)
-	public void delPet() {
-		Response response =
-				given().contentType(ContentType.JSON)
-				.when().pathParam("petId", 77232)
-				.delete(Root_URI+"/{petId}");
-		
-		//Assertion
-		response.then().body("code", equalTo(200));
-	//	response.then().body("message", equalTo("77232"));
-		System.out.println("Deleting pet");
-		System.out.println(response.getBody().asPrettyString());
-	}
+	final static String ROOT_URI = "https://petstore.swagger.io/v2/pet";
+  @Test(priority=1)
+  public void addNewPet() {
+	  String reqBody = "{"
+	            + "\"id\": 77232,"
+	            + "\"name\": \"Riley\","
+	            + " \"status\": \"alive\""
+	        + "}";
+	 
+	        Response response = 
+	            given().contentType(ContentType.JSON) // Set headers
+	            .body(reqBody) // Add request body
+	            .when().post(ROOT_URI); // Send POST request
+	 
+	        // Assertion
+	        response.then().body("id", equalTo(77232));
+	        response.then().body("name", equalTo("Riley"));
+	        response.then().body("status", equalTo("alive"));
+	    }
+	 
+  @Test(priority=2)
+  public void getPetInfo() {
+      Response response = 
+          given().contentType(ContentType.JSON) // Set headers
+          .when().pathParam("petId", "77232") // Set path parameter
+          .get(ROOT_URI + "/{petId}"); // Send GET request
+
+      // Assertion
+      response.then().body("id", equalTo(77232));
+      response.then().body("name", equalTo("Riley"));
+      response.then().body("status", equalTo("alive"));
+  }
+  
+  @Test(priority=3)
+  public void deletePet() {
+      Response response = 
+          given().contentType(ContentType.JSON) // Set headers
+          .when().pathParam("petId", "77232") // Set path parameter
+          .delete(ROOT_URI + "/{petId}"); // Send DELETE request
+
+      // Assertion
+      response.then().body("code", equalTo(200));
+      response.then().body("message", equalTo("77232"));
+  }
 
 }
