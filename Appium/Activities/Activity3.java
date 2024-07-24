e996a85baee68229aaf693b13f898d72f476826c
Srilakshmi-poka <srilpok1@in.ibm.com>
2024-07-24 18:36:14 +0530

    Update Activity3.java

--- Appium/Activities/Activity3.java
+++ Appium/Activities/Activity3.java
@@ -1,77 +1,115 @@
-package examples;
+package Activities;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
+import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+
 public class Activity3 {
-	AndroidDriver driver;
+	
+	    
+	    AndroidDriver driver;
 
-	@BeforeClass
-	public void setup() throws MalformedURLException {
-		UiAutomator2Options options=new UiAutomator2Options();
-		options.setPlatformName("android");
-		options.setAutomationName("UiAutomator2");
-		options.setAppPackage("com.google.android.keep");
-		options.setAppActivity(".activities.BrowseActivity");
-		options.noReset();
-		@SuppressWarnings("deprecation")
-		URL serverURL=new URL("http://localhost:4723/");
-		driver=new AndroidDriver(serverURL,options);
-	}
+	    
+	    @BeforeClass
+	    public void setUp() throws MalformedURLException {
+	        // Desired Capabilities
+	        UiAutomator2Options options = new UiAutomator2Options();
+	        options.setPlatformName("android");
+	        options.setAutomationName("UiAutomator2");
+	        options.setAppPackage("com.google.android.calculator");
+	  	  options.setAppActivity("com.android.calculator2.Calculator");
+	  	  options.noReset();
 
-		
-	@Test
-	public void authOperations() {
-			driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_5")).click();
-			driver.findElement(AppiumBy.accessibilityId("plus")).click();
-			driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_9")).click();
-			driver.findElement(AppiumBy.accessibilityId("equals")).click();
-			String result=driver.findElement(AppiumBy.id("com.google.android.calculator:id/result_final")).getText();
-			System.out.println("addition of 5+9 : " + result);
-			Assert.assertEquals(result, "14");
-	}
+	        // Server Address
+	        URL serverURL = new URL("http://localhost:4723/");
 
-	@Test
-	public void subtraction() {
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_1")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_0")).click();
-		driver.findElement(AppiumBy.accessibilityId("minus")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_5")).click();
-		driver.findElement(AppiumBy.accessibilityId("equals")).click();
-		String subtract=driver.findElement(AppiumBy.id("com.google.android.calculator:id/result_final")).getText();
-		System.out.println("Subtraction of 10-5 is : " +subtract);
-		Assert.assertEquals(subtract, "5");
-	}
+	        // Driver Initialization
+	        driver = new AndroidDriver(serverURL, options);
+	    }
 
-	@Test
-	public void multiplication() {
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_5")).click();
-		driver.findElement(AppiumBy.accessibilityId("multiply")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_1")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_0")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_0")).click();
-		driver.findElement(AppiumBy.accessibilityId("equals")).click();
-		String multiply=driver.findElement(AppiumBy.id("com.google.android.calculator:id/result_final")).getText();
-		System.out.println("Multiplication of 5*100 is : " + multiply);
-		Assert.assertEquals(multiply, "500");
+	   
+	    @Test
+	    public void additionTest() {
+	        // Perform the calculation
+	        driver.findElement(AppiumBy.id("digit_5")).click();
+	        driver.findElement(AppiumBy.accessibilityId("plus")).click();
+	        driver.findElement(AppiumBy.id("digit_9")).click();
+	        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+	        // Find the result
+	        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+	        // Assertion
+	        Assert.assertEquals(result, "14");
+	    }
+
+	    
+	    @Test
+	    public void subtractTest() {
+	        // Perform the calculation
+	        driver.findElement(AppiumBy.id("digit_1")).click();
+	        driver.findElement(AppiumBy.id("digit_0")).click();
+	        driver.findElement(AppiumBy.accessibilityId("minus")).click();
+	        driver.findElement(AppiumBy.id("digit_5")).click();
+	        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+	        // Find the result
+	        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+	        // Assertion
+	        Assert.assertEquals(result, "5");
+	    }
+
+	    // Test method
+	    @Test
+	    public void multiplyTest() {
+	        // Perform the calculation
+	        driver.findElement(AppiumBy.id("digit_5")).click();
+	        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+	        driver.findElement(AppiumBy.id("digit_1")).click();
+	        driver.findElement(AppiumBy.id("digit_0")).click();
+	        driver.findElement(AppiumBy.id("digit_0")).click();
+	        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+	        // Find the result
+	        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+	        // Assertion
+	        Assert.assertEquals(result, "500");
+	    }
+
+	    // Test method
+	    @Test
+	    public void divideTest() {
+	        // Perform the calculation
+	        driver.findElement(AppiumBy.id("digit_5")).click();
+	        driver.findElement(AppiumBy.id("digit_0")).click();
+	        driver.findElement(AppiumBy.accessibilityId("divide")).click();
+	        driver.findElement(AppiumBy.id("digit_2")).click();
+	        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+	        // Find the result
+	        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+	        // Assertion
+	        Assert.assertEquals(result, "25");
+	    }
+
+	   
+	    @AfterClass
+	    public void tearDown() {
+	       
+	        driver.quit();
+	    }
 	}
-	@Test
-	public void division() {
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_5")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_0")).click();
-		driver.findElement(AppiumBy.accessibilityId("divide")).click();
-		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_2")).click();
-		driver.findElement(AppiumBy.accessibilityId("equals")).click();
-		String division=driver.findElement(AppiumBy.id("com.google.android.calculator:id/result_final")).getText();
-		System.out.println("division of 50/2 is : " +division);
-		Assert.assertEquals(division, "25");
-	}
-}
